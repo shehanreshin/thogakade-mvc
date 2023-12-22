2a771afd17438ee07e0dd48cd3eb803d2aa7cbfc
shehanreshin <asp.shehanreshin@gmail.com>
2023-12-22 23:10:38 +0530

    feat: connect data to combo boxes in place order form

--- thogakade-mvc/src/main/java/controller/PlaceOrdersController.java
+++ thogakade-mvc/src/main/java/controller/PlaceOrdersController.java
@@ -1,34 +1,108 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import dto.CustomerDTO;
+import dto.ItemDTO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.CustomerModel;
+import model.ItemModel;
+import model.impl.CustomerModelImpl;
+import model.impl.ItemModelImpl;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class PlaceOrdersController {
-    @FXML
-    private JFXButton btnNotifications;
-    @FXML
-    private JFXButton btnLogout;
-    @FXML
-    private JFXButton btnEdit;
-    @FXML
-    private JFXButton btnSettings;
-    @FXML
-    private JFXButton btnPlaceOrders;
-    @FXML
-    private JFXButton btnCustomers;
-    @FXML
-    private JFXButton btnOrders;
-    @FXML
-    private JFXButton btnItems;
+public class PlaceOrdersController implements Initializable {
     @FXML
     private JFXButton btnDashboard;
+
+    @FXML
+    private JFXButton btnItems;
+
+    @FXML
+    private JFXButton btnOrders;
+
+    @FXML
+    private JFXButton btnCustomers;
+
+    @FXML
+    private JFXButton btnPlaceOrders;
+
+    @FXML
+    private JFXButton btnSettings;
+
+    @FXML
+    private JFXButton btnLogout;
+
+    @FXML
+    private JFXButton btnNotifications;
+
+    @FXML
+    private JFXButton btnEdit;
+
+    @FXML
+    private JFXTextField txtDescription;
+
+    @FXML
+    private JFXTextField txtUnitPrice;
+
+    @FXML
+    private JFXTextField txtQty;
+
+    @FXML
+    private TableView tblOrders;
+
+    @FXML
+    private TableColumn colCode;
+
+    @FXML
+    private TableColumn colDescription;
+
+    @FXML
+    private TableColumn colQty;
+
+    @FXML
+    private TableColumn colAmount;
+
+    @FXML
+    private TableColumn colOption;
+
+    @FXML
+    private JFXButton btnPlaceOrder;
+
+    @FXML
+    private JFXButton btnAddToCart;
+
+    @FXML
+    private JFXComboBox cmbxCustomerId;
+
+    @FXML
+    private JFXComboBox cmbxItemCode;
+
+    @FXML
+    private JFXTextField txtName;
+
+    private CustomerModel customerModel = new CustomerModelImpl();
+    private ItemModel itemModel = new ItemModelImpl();
+    private List<ItemDTO> items;
+    private List<CustomerDTO> customers;
+
 
     public void notificationsButtonOnAction() {
     }
@@ -70,5 +144,46 @@ public class PlaceOrdersController {
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/Home.fxml"))));
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        colCode.setCellValueFactory(new PropertyValueFactory<>("code"));
+        colDescription.setCellValueFactory(new PropertyValueFactory<>("desc"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        colAmount.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        colOption.setCellValueFactory(new PropertyValueFactory<>("btn"));
+
+        try {
+            customers = customerModel.allCustomers();
+            items = itemModel.allItems();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        loadItemCodes();
+        loadCustomerIds();
+    }
+
+    private void loadItemCodes() {
+        ObservableList list = FXCollections.observableArrayList();
+
+        for (ItemDTO itemDTO : items) {
+            list.add(itemDTO.getCode());
+        }
+
+        cmbxItemCode.setItems(list);
+    }
+
+    private void loadCustomerIds() {
+        ObservableList list = FXCollections.observableArrayList();
+
+        for (CustomerDTO customerDTO : customers) {
+            list.add(customerDTO.getId());
+        }
+
+        cmbxCustomerId.setItems(list);
     }
 }
